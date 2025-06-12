b56949882aa0d965974be6266f6535bfd8281a72
Humarr <74363540+Humarr@users.noreply.github.com>
2025-06-12 22:03:48 +0100

    create sixth sales page

--- src/components/sales6/Hook.tsx
+++ src/components/sales6/Hook.tsx
@@ -6,10 +6,10 @@ const HookSection = () => {
     <section className="section-py bg-background">
     <div className="container-narrow">
       <h3 className=" text-center mb-8 mt-8">
-      What If You Could Make <span className='bg-accent text-white px-2 py-1 rounded'>Your First ₦10K  </span>This Week…
+      What If You Could Make <span className='bg-accent text-white px-2 py-1 rounded mx-2'>Your First ₦10K  </span>This Week…
       </h3>
       <h1 className="text-gradient text-center mb-8">
-        Without Posting <span className='bg-accent text-white px-2 py-1 rounded'> ‘Please Support My Business’ </span>on WhatsApp?
+        Without Posting <span className='text-accent font-medium italic'> ‘Please Support My Business’ </span>on WhatsApp?
       </h1>
       {/* <h1 className="text-gradient text-center mb-8">
         What If You Could Make Your First ₦10K This Week… Without Posting ‘Please Support My Business’ on WhatsApp?
