e351abf074bda70a7e11d4a09cbbf1ae2da55a88
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-05 12:59:54 +0100

    Add sales9 pages

--- src/components/business/Congratulations.tsx
+++ src/components/business/Congratulations.tsx
@@ -20,7 +20,7 @@ export default function Congratulations() {
     return (
         <>
         <section className="bg-background text-primary px-6 py-12 text-center space-y-6 mb-16">
-            <p className="text-3xl font-bold uppercase text-accent mb-12">CONGRATULATIONS, {user.name}!</p>
+            <p className="text-3xl font-bold uppercase text-accent mb-12">CONGRATULATIONS, <span className="font-normal text-primary">{user.name}!</span></p>
             <p>You've taken the BOLD STEP <br/> I present to you....</p>
             
 
