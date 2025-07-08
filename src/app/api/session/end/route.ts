c54e1f408aca3c0d61bbb187a7583a7a125b3bb5
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-09 00:29:47 +0100

    Add tracking to biz page

--- src/app/api/session/end/route.ts
+++ src/app/api/session/end/route.ts
@@ -3,7 +3,7 @@ import { prisma } from '@/lib/prisma'
 
 export async function POST(req: NextRequest) {
     const { sessionId } = await req.json()
-
+    console.log("sessionId: ", sessionId)
   await prisma.visitorSession.update({
     where: { id: sessionId },
     data: { leftAt: new Date() },
