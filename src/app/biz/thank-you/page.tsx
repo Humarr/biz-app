14f3c9ccfc900bd81b7f065598ba312524fd4700
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-10 06:08:40 +0100

    Add tracking to biz page

--- src/app/biz/thank-you/page.tsx
+++ src/app/biz/thank-you/page.tsx
@@ -1,9 +1,12 @@
 import ThankYouTracker from "@/components/biz/ThankYouTracker";
+import { Suspense } from "react";
 
 export default function ThankYouPage() {
   return (
     <main>
-      <ThankYouTracker />
+        <Suspense fallback={<div>Loading...</div>}>
+            <ThankYouTracker />
+        </Suspense>
     </main>
   );
 }
