982cacc8d227e387056a89a9f537c18a53f6e15a
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-26 19:38:10 +0100

    Add cash-o-logy

--- src/app/cashology/congrats/page.tsx
+++ src/app/cashology/congrats/page.tsx
@@ -1,8 +1,9 @@
+'use client'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 
-export default function Success() {
+export default function Congrats() {
   const router = useRouter();
   const { reference } = router.query;
 
