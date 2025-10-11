5984e7e49938f427e3c248151d13538f879dcf06
Humarr <74363540+Humarr@users.noreply.github.com>
2025-10-11 08:26:37 +0100

    buildyourbiz

--- src/app/startbiz/new/page.tsx
+++ src/app/startbiz/new/page.tsx
@@ -27,7 +27,8 @@ export default function SalesPage2() {
           </h4>
 
           <p className="font-semibold">
-            Almost everything you know about starting a business is wrong.
+            Almost everything most people say about starting a business is wrong.
+            {/* Almost everything you know about starting a business is wrong. */}
             {/* I am writing to you today because I have something important to share with you. */}
           </p>
           <p>
